9c555f2d01cbefbdcd679fa9fc6a3bcc87b85e88
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-08-07 14:33:19 -0400

    Add/update test_17.py file

--- tests/test_17.py
+++ tests/test_17.py
@@ -1,5 +1,5 @@
 import pytest
-from definition_28c4057f83a94fb4bbe5b279e18fc36f import fit_pit_overlay
+from definition_c6c62c2a1e8c401288f9bed6c25ac5c6 import fit_pit_overlay
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -10,28 +10,26 @@ def test_fit_pit_overlay_empty_input():
     with pytest.raises(ValueError):
         fit_pit_overlay(X_train, y_train)
 
-def test_fit_pit_overlay_no_macro_factors():
-    X_train = pd.DataFrame({'const': [1, 1, 1]})  # No macroeconomic factors
-    y_train = pd.Series([0.1, 0.2, 0.3])
+def test_fit_pit_overlay_successful_fit():
+    X_train = pd.DataFrame({'macro_variable': [1, 2, 3, 4, 5]})
+    y_train = pd.Series([0.1, 0.2, 0.3, 0.4, 0.5])
     model = fit_pit_overlay(X_train, y_train)
     assert isinstance(model, LinearRegression)
-
-def test_fit_pit_overlay_valid_input():
-    X_train = pd.DataFrame({'macro1': [1, 2, 3], 'macro2': [4, 5, 6]})
-    y_train = pd.Series([0.1, 0.2, 0.3])
-    model = fit_pit_overlay(X_train, y_train)
-    assert isinstance(model, LinearRegression)
-    model.fit(X_train, y_train)
-    assert model.coef_.shape == (2,)  # Check that the model has coefficients for the features.
-
-def test_fit_pit_overlay_non_numeric_features():
-    X_train = pd.DataFrame({'macro1': ['a', 'b', 'c'], 'macro2': [4, 5, 6]})
-    y_train = pd.Series([0.1, 0.2, 0.3])
-    with pytest.raises(TypeError):
-        fit_pit_overlay(X_train, y_train)
 
 def test_fit_pit_overlay_mismatched_lengths():
-    X_train = pd.DataFrame({'macro1': [1, 2, 3], 'macro2': [4, 5, 6]})
-    y_train = pd.Series([0.1, 0.2])  # Different length
+    X_train = pd.DataFrame({'macro_variable': [1, 2, 3]})
+    y_train = pd.Series([0.1, 0.2, 0.3, 0.4])
     with pytest.raises(ValueError):
         fit_pit_overlay(X_train, y_train)
+
+def test_fit_pit_overlay_nan_values():
+    X_train = pd.DataFrame({'macro_variable': [1, 2, np.nan, 4, 5]})
+    y_train = pd.Series([0.1, 0.2, 0.3, 0.4, 0.5])
+    with pytest.raises(ValueError):
+        fit_pit_overlay(X_train, y_train)
+
+def test_fit_pit_overlay_zero_variance():
+    X_train = pd.DataFrame({'macro_variable': [1, 1, 1, 1, 1]})
+    y_train = pd.Series([0.1, 0.2, 0.3, 0.4, 0.5])
+    model = fit_pit_overlay(X_train, y_train)
+    assert isinstance(model, LinearRegression)
